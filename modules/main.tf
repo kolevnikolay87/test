0c5b62e2ecfd9f8fb9fb51d8c66899d32e91dfd7
Nikolay <nikolay_kolev@flutterint.com>
2023-06-14 14:09:41 +0300

    wkrjgnerw

---
+++ modules/main.tf
@@ -0,0 +1,7 @@
+module "terraform_test_module" {
+ source  = "github.com/kolevnikolay87/test.git?ref=1.0.0"
+ 
+#  argument_1                     = var.test_1
+#  argument_2                     = var.test_2
+#  argument_3                     = var.test_3
+}
