fb5f0856b41e2fb21ccea1716d3dc3276008170c
Nikolay <nikolay_kolev@flutterint.com>
2023-06-14 14:15:01 +0300

    asbaeg

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,7 @@
 module "terraform_test_module" {
  source  = "github.com/kolevnikolay87/test.git?ref=1.0.0"
  
-#  argument_1                     = var.test_1
+   vpc_cidr                     = ""
 #  argument_2                     = var.test_2
 #  argument_3                     = var.test_3
 }
